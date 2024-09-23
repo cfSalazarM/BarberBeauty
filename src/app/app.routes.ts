8fe4b97aa6a5686c576c85d31e469d61a13c90f3
Yeison Micolta <ymicolta@udenar.edu.co>
2024-09-23 18:11:38 -0500

    Redireccionamiento Parcial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeWrapperComponent } from '@modules/home/pages/home-wrapper/home-wrapper.component';
+import { BarberShopPageComponent } from '@modules/BarberShop/pages/barber-shop-page/barber-shop-page.component';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from '@modules/auth/pages/login-page/login-page.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
+
 export const routes: Routes = [
     {
+        path: 'home',
+        component: HomePageComponent,
+
+    },
+    {
         path: 'auth',
+        component: LoginPageComponent,
         loadChildren: () => import('./modules/auth/auth.routes').then(m => m.authRoutes)
     },
     {
@@ -10,5 +22,10 @@ export const routes: Routes = [
         component: HomeWrapperComponent,
         loadChildren: () => import('./modules/home/pages/home-wrapper/homew.routes').then(m => m.homeWRoutes),
         
+    },
+    {
+        path:'BarberShop',
+        component: BarberShopPageComponent,
+        loadChildren: () => import('./modules/BarberShop/pages/barber-shop-page/barbershop.routes').then(m => m.barberShopRoutes),
     }
 ];
